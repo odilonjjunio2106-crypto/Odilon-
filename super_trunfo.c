f84a450ae75bfa9b4e03c286a0ae2574aa6ffeec
odilonjjunio2106-crypto <odilonjjunio2106@gmail.com>
2025-11-12 14:01:46 -0300

    Update super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -8,3 +8,10 @@ int main() {
     float pib = 699.28;              // bilhões de reais
     int numPontosTuristicos = 50;
 
+    char estado = 'B';
+    char codigo[] = "B02";
+    char nome[] = "RIO DE JANEIRO";
+    int populacao = 6748000;
+    float area = 1200.25;            // km²
+    float pib = 300.50;              // bilhões de reais
+    int numPontosTuristicos = 30;
