62513f5281dd620aa6593e5d3700d297d1de5c83
odilonjjunio2106-crypto <odilonjjunio2106@gmail.com>
2025-11-12 13:56:04 -0300

    super_trunfo.c
    atividade

---
+++ super_trunfo.c
@@ -0,0 +1,10 @@
+int main() {
+    // Dados da cidade
+    char estado = 'A';
+    char codigo[] = "A01";
+    char nome[] = "São Paulo";
+    int populacao = 12325000;
+    float area = 1521.11;            // km²
+    float pib = 699.28;              // bilhões de reais
+    int numPontosTuristicos = 50;
+
