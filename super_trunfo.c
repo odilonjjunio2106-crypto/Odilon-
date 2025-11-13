fabfadcfaf2c4ffedc7a4ec5d912b241cc976e7a
odilonjjunio2106-crypto <odilonjjunio2106@gmail.com>
2025-11-13 11:09:39 -0300

    Update super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,17 +1,20 @@
+#include <stdio.h>
+
 int main() {
     // Dados da cidade
     char estado = 'A';
     char codigo[] = "A01";
     char nome[] = "São Paulo";
     int populacao = 12325000;
-    float area = 1521.11;            // km²
-    float pib = 699.28;              // bilhões de reais
+    float area = 1521.11 // km²;
+    float pib = 699.28 // bilhões de reais;
     int numPontosTuristicos = 50;
 
     char estado = 'B';
     char codigo[] = "B02";
     char nome[] = "RIO DE JANEIRO";
     int populacao = 6748000;
-    float area = 1200.25;            // km²
-    float pib = 300.50;              // bilhões de reais
+    float area = 1200.25 // km²;            // km²
+    float pib = 300.50 // bilhões de reais;
     int numPontosTuristicos = 30;
+}
